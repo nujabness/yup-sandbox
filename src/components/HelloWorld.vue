c620952f962664bef6e6c0accfc5573a71db429b
Mohammed EL ASSOURI <mohammed.elassouri@abbeal.com>
2022-05-17 13:54:21 +0200

    Init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-form class="hello">
+      <h1>Yup SandBox</h1>
+
+      <v-text-field
+          v-model="root['myField']['question']"
+      />
+
+      {{ errors }}
+      <br />
+
+      <br />
+      {{ myField }}
+    </v-form>
+</template>
+
+<script setup>
+import { useForm, useField } from 'vee-validate';
+import { ref } from 'vue';
+import { buildYup } from 'schema-to-yup';
+
+const schema = {
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "type": "object",
+  "required": ["myField"],
+  "properties": {
+    "myField": {
+      "type": "object",
+      "required": ["question"],
+      "properties": {
+        "question": {
+          "type": "string",
+          "enum": [
+            "value1",
+            "other"
+          ]
+        }
+      }
+    }
+  }
+}
+const root = ref({});
+
+const { errors } = useForm(
+    {
+      validationSchema: buildYup(schema, {})
+    }
+);
+const { value } =  useField(
+    'myField',
+    undefined,
+);
+root.value['myField'] = value;
+root.value['myField'] = {};
+
+</script>
